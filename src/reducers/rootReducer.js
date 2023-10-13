1a730546a779d2b711856f30f84f72ff46686d71
Moshe Frankipour <moshe.frankipour@intel.com>
2023-10-13 13:29:03 +0300

    Remove react project (#2)

--- src/reducers/rootReducer.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux';
-import { userReducer } from './userReducer';
-import { offersReducer } from './offersReducer';
-
-export const rootReducer = combineReducers({
-    user: userReducer,
-    offers: offersReducer,
-    // other reducers will go here
-});
