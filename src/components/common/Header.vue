2e33a372af57e7b07e77252341df035701d899db
Ben Avrahami <67713588+Ben-Avrahami@users.noreply.github.com>
2023-10-13 18:58:46 +0300

    add homePage (#12)
    * make header open from the right
    
    * add logo
    
    * add homeview

--- src/components/common/Header.vue
+++ src/components/common/Header.vue
@@ -1,49 +1,34 @@
 <template>
     <v-app-bar app color="#559ccb">
-        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
         <v-toolbar-title>SuperUs</v-toolbar-title>
+        <v-app-bar-nav-icon @click.stop="drawer = !drawer"></v-app-bar-nav-icon>
     </v-app-bar>
 
-    <v-navigation-drawer app temporary v-model="drawer">
+    <v-navigation-drawer app temporary v-model="drawer" location="right">
         <v-list>
 
             <v-list-item link to="/">
-                <v-list-item-icon>
                     <v-icon>mdi-home</v-icon>
-                </v-list-item-icon>
-                <v-list-item-content>
                     <v-list-item-title>עמוד הבית</v-list-item-title>
-                </v-list-item-content>
             </v-list-item>
 
             <template v-if="true">
 
                 <v-list-item link to="/login">
-                    <v-list-item-icon>
                         <v-icon>mdi-login</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
                         <v-list-item-title>התחברות</v-list-item-title>
-                    </v-list-item-content>
                 </v-list-item>
             </template>
+
                 <v-list-item link to="/register">
-                    <v-list-item-icon>
                         <v-icon>mdi-account-multiple-plus</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
                         <v-list-item-title>הרשמה</v-list-item-title>
-                    </v-list-item-content>
                 </v-list-item>
 
 
                 <v-list-item link to="/about">
-                    <v-list-item-icon>
                         <v-icon>mdi-information</v-icon>
-                    </v-list-item-icon>
-                    <v-list-item-content>
                         <v-list-item-title>אודות</v-list-item-title>
-                    </v-list-item-content>
                 </v-list-item>
 
         </v-list>
