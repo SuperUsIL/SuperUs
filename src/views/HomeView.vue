bf3ae35acb561ee7019a18f0bb2587a3ad689af6
Ben-avrahami <ben.avrahami@intel.com>
2023-10-13 18:54:18 +0300

    add homeview

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,7 +1,21 @@
 <template>
   <div class="home">
     <v-img src="./assets/SuperUs-logo.png" class="mr-2 logo" max-height=auto max-width="250"></v-img>
-    <h1>Hi</h1>
+    <v-card class="rounded-card" >
+          <v-card-title>SuperUs</v-card-title>
+          <v-card-subtitle>המיזם נועד לקשר בין אנשים אשר צריכים תרומה לאנשים היכולים לספק את התרומה ולשנע אותה</v-card-subtitle>
+          <v-card-text>
+אנו מקווים כי השימוש במערכת זו יוכל לעזור לציוד להגיע למקום בו הצורך הינו הגדול ביותר ובצורה המיטבית
+          </v-card-text>
+          <v-card-actions class="justify-center">
+            <v-btn class="white--text"
+              style="height: 50px;width: 100px;font-weight: bold; font-size: 17px ;background-color: #559ccb"
+               link to="/register">הרשמה</v-btn>
+            <v-btn class="white--text"
+              style="height: 50px;width: 100px;font-weight: bold; font-size: 17px ;background-color: #559ccb"
+              link to="/login">התחברות</v-btn>
+          </v-card-actions>
+        </v-card>
   </div>
 </template>
 
@@ -13,3 +27,20 @@ export default defineComponent({
   },
 });
 </script>
+
+<style scoped>
+.home {
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+}
+.rounded-card {
+  background-color: #d9d9d9;
+  border-radius: 15px;
+  padding: 20px;
+  text-align: center;
+  overflow: auto;
+}
+
+</style>
